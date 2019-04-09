c63c9afdcf8de96e84d27192d0a3923a10976606
mudzadha <mudzadha@gmail.com>
2019-04-09 17:43:31 +0800

    add folder detailpage add file detailpage

---
+++ src/app/detailpage/detail-model.ts
@@ -0,0 +1,8 @@
+export interface Detail {
+  album_id: string;
+  id: string;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+
+}
--- src/app/detailpage/detail-page.ts
+++ src/app/detailpage/detail-page.ts
@@ -1,12 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component ( {
-  selector : 'trg-details',
-  templateUrl : './details.html',
+  selector : 'detail-page',
+  templateUrl : './detail-page.html',
 
 })
-export class Details{
-  constructor(){
+export class JokePage implements OnInit,OnDestroy{
+
+  joke$ : Observable<Joke>; //TODO i added this
+
+  constructor(private jokeservice :JokeService, private router : Router){
+
+    this.joke$ = this.jokeservice.findJoke();
+    console.log("Constructor is running ...")
+
+    // this.joke$.subscribe(data=>{
+    //   console.log(data)
+    // })
+
+  }
+  goToNextPage(){
+    this.router.navigate(['/academy/apprentices/',new Date().getTime()]);
+    console.log("Push me")
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy is running ...")
+  }
+  ngOnInit(): void {
+    console.log("ngOnInit is running ...")
 
   }
 }
