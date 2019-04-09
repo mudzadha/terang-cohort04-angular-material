ca44959cb55dbf6331bf2d65bedcb7901fe182ee
Nate C <N_t_N@live.com>
2019-04-09 18:11:39 +0800

    fix user-model

--- src/app/user/user-model.ts
+++ src/app/user/user-model.ts
@@ -3,20 +3,27 @@ export interface User {
   "name": string;
   "username": string;
   "email": string;
-  "address": {
-    "street": string;
-    "suite": string;
-    "city": string;
-    "zipcode": string;
-    "geo": {
-      "lat": string;
-      "lng": number;
-    }
-  };
   "phone": string;
   "website": string;
-  "company": {
+  "address": Address;
+  "company": Company;
+  }
+
+export interface Address {
+  "street": string;
+  "suite": string;
+  "city": string;
+  "zipcode": string;
+  geo: Geo;
+}
+
+export interface Geo {
+      "lat": string;
+      "lng": string;
+    }
+
+export interface Company {
     "name": string;
     "catchPhrase": string;
     "bs": string;
-};
+    }
