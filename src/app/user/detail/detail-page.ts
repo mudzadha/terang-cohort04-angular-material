fc87ff7a1eafa9bdb5091d71ef28c75e9e999ec7
mudzadha <mudzadha@gmail.com>
2019-04-10 11:17:05 +0800

    add folder detailpage add file detailpage

--- src/app/user/detail/detail-page.ts
+++ src/app/user/detail/detail-page.ts
@@ -3,6 +3,7 @@ import {Observable} from 'rxjs';
 import {ActivatedRoute, Router} from '@angular/router';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {UserService} from '../../../services/user.service';
+import {User} from '../user-model';
 
 @Component ( {
   selector : 'trg-photo-detail',
@@ -14,16 +15,16 @@ export class UsersDetailPage implements OnInit{
 
   mainForm: FormGroup;
   submitted: false;
-  user$ : Observable(User) = null;
+  user$ : Observable<User> = null;
 
   constructor(private form: FormBuilder,
               private userService : UserService,
               private route : ActivatedRoute
               ){
 
-    this.route.params.subscribe((param :(id: string)) =>{
+    this.route.params.subscribe((param :{id: number}) =>{
       this.user$ = userService.findUserByID(param.id)
-    })
+    });
 
   }
   ngOnInit() {
